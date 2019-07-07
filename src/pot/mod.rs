9404c8c270d69bfa9b6f7630018b548f1752752a
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2019-07-07 22:17:51 +0200

    Simplify path management

--- src/pot/mod.rs
+++ src/pot/mod.rs
@@ -2,7 +2,7 @@ use std::default::Default;
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::Ipv4Addr;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process::Command;
 use std::str::FromStr;
 use walkdir::WalkDir;
@@ -26,61 +26,41 @@ pub enum PotError {
     FileError,
 }
 
+// get pot prefix in the same way as pot does:
+// find PREFIX/bin/pot and get the PREFIX
 fn get_pot_prefix() -> Result<PathBuf, PotError> {
-    let pathname = match Command::new("which").arg("pot").output() {
-        Ok(s) => s,
-        Err(_) => return Err(PotError::WhichError),
-    };
-    let pot_path = match String::from_utf8(pathname.stdout) {
-        Ok(s) => s,
-        Err(_) => return Err(PotError::WhichError),
-    };
-    let pot_path = Path::new(pot_path.trim_end());
-    let pot_prefix = match pot_path.parent() {
-        Some(i) => i,
-        _ => return Err(PotError::PathError),
-    };
-    let pot_prefix = match pot_prefix.parent() {
-        Some(i) => i,
-        _ => return Err(PotError::PathError),
-    };
+    let pathname = Command::new("which")
+        .arg("pot")
+        .output()
+        .map_err(|_| PotError::WhichError)?;
+    let pot_path =
+        PathBuf::from(String::from_utf8(pathname.stdout).map_err(|_| PotError::WhichError)?);
+    let pot_prefix = pot_path.parent().ok_or(PotError::PathError)?;
+    let pot_prefix = pot_prefix.parent().ok_or(PotError::PathError)?;
     Ok(pot_prefix.to_path_buf())
 }
 
 fn get_conf_default() -> Result<String, PotError> {
-    let mut pot_conf = match get_pot_prefix() {
-        Ok(p) => p,
-        Err(e) => return Err(e),
-    };
+    let mut pot_conf = get_pot_prefix()?;
     pot_conf.push("etc");
     pot_conf.push("pot");
     pot_conf.push("pot.default.conf");
 
-    let mut conf_file = match File::open(pot_conf.as_path()) {
-        Ok(x) => x,
-        Err(_) => return Err(PotError::FileError),
-    };
+    let mut conf_file = File::open(pot_conf.as_path()).map_err(|_| PotError::FileError)?;
     let mut conf_str = String::new();
-    match conf_file.read_to_string(&mut conf_str) {
-        Ok(_) => (),
-        Err(_) => return Err(PotError::FileError),
-    }
+    let _ = conf_file
+        .read_to_string(&mut conf_str)
+        .map_err(|_| PotError::FileError)?;
     Ok(conf_str)
 }
 
 fn get_conf() -> Result<String, PotError> {
-    let mut pot_conf = match get_pot_prefix() {
-        Ok(p) => p,
-        Err(e) => return Err(e),
-    };
+    let mut pot_conf = get_pot_prefix()?;
     pot_conf.push("etc");
     pot_conf.push("pot");
     pot_conf.push("pot.conf");
 
-    let mut conf_file = match File::open(pot_conf.as_path()) {
-        Ok(x) => x,
-        Err(_) => return Err(PotError::FileError),
-    };
+    let mut conf_file = File::open(pot_conf.as_path()).map_err(|_| PotError::FileError)?;
     let mut conf_str = String::new();
     match conf_file.read_to_string(&mut conf_str) {
         Ok(_) => (),
